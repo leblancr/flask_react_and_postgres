ce955806f1ef44e26a427c82109916da6e348ecd
rich <rkba2@outlook.com>
2023-04-13 20:19:57 -0400

    Added more files to git.

---
+++ backend/app.py
@@ -0,0 +1,83 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rich:admin@localhost/baby-tracker'
+db = SQLAlchemy(app)
+# db.init_app(app)  # first time only
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return f"Event: {self.description}"
+    
+    def __init__(self, description):
+        self.description = description
+        
+        
+def format_event(event):
+    return {
+        "description": event.description,
+        "id": event.id,
+        "created_at": event.created_at
+    }
+        
+
+@app.route('/')
+def hello():
+    return 'hello'
+
+
+# create an event
+@app.route('/event', methods=['POST'])
+def create_event():
+    description = request.json['description']
+    event = Event(description)
+    db.session.add(event)
+    db.session.commit()
+    return format_event(event)
+
+
+# get all event
+@app.route('/events', methods=['Get'])
+def get_events():
+    events = Event.query.order_by(Event.id.asc()).all()
+    event_list = []
+    for event in events:
+        event_list.append(format_event(event))
+    return {"event": event_list}
+        
+# get one event
+@app.route('/event/<event_id>', methods=['Get'])
+def get_event(event_id):
+    event = Event.query.filter_by(id=event_id).one()
+    return {'event': format_event(event)}
+    
+    
+# delete event
+@app.route('/event/<event_id>', methods=['DELETE'])
+def delete_event(event_id):
+    event = Event.query.filter_by(id=event_id).one()
+    db.session.delete(event)
+    db.session.commit()
+    return f"Event: (id: {id}) deleted"
+
+
+# edit event
+@app.route('/event/<event_id>', methods=['PUT'])
+def update_event(event_id):
+    event = Event.query.filter_by(id=event_id)
+    description = request.json['description']
+    event.update(dict(description=description, created_at=datetime.utcnow()))
+    db.session.commit()
+    return {'event': format_event(event.one())}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
